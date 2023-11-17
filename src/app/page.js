dfc7a5fbf4aa65777cd580a0ec1b68d4bfc751c5
BMAgeas <Ben.Mullan@Ageas.Co.UK>
2023-11-17 16:10:24 +0000

    feature/FT-2-products-loading-from-json

--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-import globalStyleVariables from "./variables.module.scss";
-
-export default function Home() {
-	return (
-		<main style={{ backgroundColor : globalStyleVariables.subtleGrey }}>
-			Content here...
-		</main>
-	);
-};
