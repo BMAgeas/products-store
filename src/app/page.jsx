9c1aff4fae73d3f131abdc03e9d61c9c05db3082
BMAgeas <Ben.Mullan@Ageas.Co.UK>
2023-11-17 16:10:37 +0000

    feature/FT-2-products-loading-from-json

---
+++ src/app/page.jsx
@@ -0,0 +1,27 @@
+import globalStyleVariables	from "./variables.module.scss";
+import {promises as fs}		from "fs";
+
+export default async function Home() {
+	
+	const productsJson = JSON.parse(await fs.readFile(process.cwd() + "/data/products.json", "utf8"));
+	
+	return (
+		<>
+			<header>Search...</header>
+			<main style={{ backgroundColor : globalStyleVariables.subtleGrey }}>
+				
+				<b>Products:</b>
+
+				{
+					productsJson.Products.map(
+						(Product, index, array) =>
+							<p>{Product.Name}</p>
+					)
+				}
+
+			</main>
+			<footer>Products Store (c) 2023</footer>
+		</>
+	);
+
+};
---
+++ src/components/ProductBox/ProductBox.jsx
@@ -0,0 +1,9 @@
+import styles from "./ProductBox.module.scss";
+
+export default function ProductBox({ ProductName }) {
+	return (
+		<div>
+			{ProductName}
+		</div>
+	);
+}
