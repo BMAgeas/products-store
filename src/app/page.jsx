ab9117d24382720a101966d9d4d50df26bd30ddd
BMAgeas <Ben.Mullan@Ageas.Co.UK>
2023-11-24 13:58:07 +0000

    Products and Applications showing

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,21 +10,48 @@ export const metadata = {
 
 export default async function Home() {
 
-	const productsJson = JSON.parse(await fileSys.readFile(process.cwd() + "/data/products.json", "utf8"));
+	const productsApplicationsJson = JSON.parse(await fileSys.readFile(process.cwd() + "/data/products.json", "utf8"));
 	
 	return (
 		<>
 			<PSHeader/>
 			<main className={styles.mainTag}>
 				
-				<h1 className={styles.sectionHeading}>Products:</h1>
-				<ul className={styles.ProductBoxesContainer}>
-					{
-						productsJson.Products.map(
-							(Product, index, array) => <ProductBox key={index} ProductName={Product.Name} />
-						)
-					}
-				</ul>
+				{/* _ Products _ */}
+				<section>
+					<h1 className={styles.sectionHeading}>Products:</h1>
+					<ul className={styles.ProductBoxesContainer}>
+						{
+							productsApplicationsJson.Products.map(
+								(product, index, array) =>
+									<ProductBox
+										key={index}
+										ProductName={product.Name}
+										DescriptionText={product.Description}
+										ActionButton={<button>Go</button>}
+									/>
+							)
+						}
+					</ul>
+				</section>
+
+				{/* _ Applications _ */}
+				<section>
+					<h1 className={styles.sectionHeading}>Applications:</h1>
+					<ul className={styles.ProductBoxesContainer}>
+						{
+							productsApplicationsJson.Applications.map(
+								(application, index, array) =>
+									<ProductBox
+										key={index}
+										ProductName={application.Name}
+										DescriptionText={application.Description}
+										ActionButton={<a href={application.URL}>Go</a>}
+									/>
+							)
+						}
+					</ul>
+				</section>
 
 			</main>
 			<footer>Products Store (c) 2023</footer>
--- src/components/ProductBox/ProductBox.jsx
+++ src/components/ProductBox/ProductBox.jsx
@@ -1,16 +1,15 @@
 import styles	from "./ProductBox.module.scss";
 import Image	from "next/image";
 
-export default function ProductBox({ ProductName }) {
+export default function ProductBox({ ProductName, DescriptionText, ActionButton }) {
 	return (
 		<li className={styles.productBoxOuter}>
-			<figure>
-				<Image src={`/Images/ProductImages/Default.jpg`} alt="Product Thumbnail" width={0} height={0} sizes="100vw" style={{ width : "100%", height : "auto" }} className={styles.productThumbnail} />
-				<figcaption>
-					<h6 className={styles.productHeading}><strong>{ProductName}</strong></h6>
-					Description text...
-				</figcaption>
-			</figure>
+			<Image src={`/Images/ProductImages/Default.jpg`} alt="Product Thumbnail" width={0} height={0} sizes="100vw" style={{ width : "100%", height : "10vw" }} className={styles.productThumbnail} />
+				<div>
+					<h3 className={styles.productHeading}>{ProductName}</h3>
+					<p className={styles.productDescription}>{DescriptionText}</p>
+					{ActionButton}
+				</div>
 		</li>
 	);
 }
